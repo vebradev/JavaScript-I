88c6fa1d8d9957a8eec9a8807b596e5ffbfc48e2
vebradev <justinas@vebra.dev>
2019-05-07 16:08:57 +0300

    Challenges & Stretch goal of function-conversion.js - Done.

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,40 @@
 // };
 // myFunction();
 
+let myFunction = () => {
+  console.log("Function was invoked!");
+};
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = (param) => {
+    return param;
+}
+anotherFunction("Example");
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => {
+    return param1 + param2;
+}
+add(1,2);
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let substract = (param1, param2) => {
+    return param1 - param2;
+}
+substract(1,2);
 
 // Stretch
 
@@ -28,3 +47,7 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map(num => num * 3);
+console.log(triple);
